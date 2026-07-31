8c1498f2f88601279ec8c82728acd961a0f2b67c
Ansori, M <orie.wancu@gmail.com>
2026-07-31 16:16:31 +0700

    Update applet.js

--- ow-workspace-switcher@ansori/applet.js
+++ ow-workspace-switcher@ansori/applet.js
@@ -16,11 +16,17 @@ class OWWorkspaceSwitcher extends Applet.Applet {
         this.settings.bind("overview_mode", "overview_mode", this.refreshApplet);
         this.settings.bind("show_indicator", "show_indicator", this.refreshApplet);
         this.settings.bind("indicator_style", "indicator_style", this.refreshApplet);
-        
-        // Bind Custom Size Settings
         this.settings.bind("use_custom_size", "use_custom_size", this.refreshApplet);
         this.settings.bind("custom_height", "custom_height", this.refreshApplet);
         this.settings.bind("custom_width", "custom_width", this.refreshApplet);
+        this.settings.bind("use_custom_opacity", "use_custom_opacity", this.refreshApplet);
+        this.settings.bind("custom_opacity", "custom_opacity", this.refreshApplet);
+        
+        // Bind Custom Wallpapers (1 to 10 dynamically)
+        this.settings.bind("enable_custom_wallpapers", "enable_custom_wallpapers", this._onWallpaperChanged);
+        for (let i = 1; i <= 10; i++) {
+            this.settings.bind(`bg_file_${i}`, `bg_file_${i}`, this._onWallpaperChanged);
+        }
 
         // Listen to Desktop Background changes
         this.bgSettings = new Gio.Settings({ schema_id: 'org.cinnamon.desktop.background' });
@@ -32,17 +38,20 @@ class OWWorkspaceSwitcher extends Applet.Applet {
         this.displaySignals = [];
         this.wsSignals = [];
         
-        // Main UI Container
         this.mainBox = new St.BoxLayout();
         this.actor.add(this.mainBox);
 
         this._connectSignals();
+        this._updateDesktopBackground();
         this.refreshApplet();
     }
 
     _connectSignals() {
         this.wmSignals.push(this.wm.connect('notify::n-workspaces', () => this._onWorkspacesChanged()));
-        this.wmSignals.push(this.wm.connect('workspace-switched', () => this.refreshApplet()));
+        this.wmSignals.push(this.wm.connect('workspace-switched', () => {
+            this._updateDesktopBackground();
+            this.refreshApplet();
+        }));
         
         this.displaySignals.push(global.display.connect('notify::focus-window', () => this.refreshApplet()));
         this.displaySignals.push(global.display.connect('restacked', () => this.refreshApplet()));
@@ -73,32 +82,68 @@ class OWWorkspaceSwitcher extends Applet.Applet {
         this.refreshApplet();
     }
 
+    _onWallpaperChanged() {
+        this._updateDesktopBackground();
+        this.refreshApplet();
+    }
+
+    _getWorkspaceWallpaper(index) {
+        if (!this.enable_custom_wallpapers) return null;
+        
+        let fileVar = this[`bg_file_${index + 1}`];
+        if (fileVar && fileVar !== "") {
+            return fileVar.startsWith('file://') ? fileVar : 'file://' + fileVar;
+        }
+        return null;
+    }
+
+    _updateDesktopBackground() {
+        if (!this.enable_custom_wallpapers) return;
+
+        let activeIndex = this.wm.get_active_workspace_index();
+        let customBg = this._getWorkspaceWallpaper(activeIndex);
+
+        if (customBg) {
+            let currentBg = this.bgSettings.get_string('picture-uri');
+            if (currentBg !== customBg) {
+                this.bgSettings.set_string('picture-uri', customBg);
+            }
+        }
+    }
+
     refreshApplet() {
         this.mainBox.destroy_all_children();
         
-        // Update Theme Class on main container
         this.mainBox.style_class = `ow-workspace-switcher ow-workspace-switcher-${this.theme}`;
 
-        // Get Desktop Background URI
-        let bgUri = this.bgSettings.get_string('picture-uri');
+        if (this.use_custom_opacity) {
+            let alpha = this.custom_opacity / 100;
+            if (this.theme === 'dark') {
+                this.mainBox.set_style(`background-color: rgba(45, 48, 56, ${alpha}); border: 1px solid rgba(20, 22, 26, ${alpha});`);
+            } else {
+                this.mainBox.set_style(`background-color: rgba(235, 238, 242, ${alpha}); border: 1px solid rgba(180, 185, 190, ${alpha});`);
+            }
+        } else {
+            this.mainBox.set_style('');
+        }
+
+        let globalBgUri = this.bgSettings.get_string('picture-uri');
         let activeIndex = this.wm.get_active_workspace_index();
 
         for (let i = 0; i < this.wm.n_workspaces; i++) {
             let isActive = (i === activeIndex);
-            let item = this._createWorkspaceItem(i, isActive, bgUri);
+            let item = this._createWorkspaceItem(i, isActive, globalBgUri);
             this.mainBox.add_child(item);
         }
     }
 
-    _createWorkspaceItem(index, isActive, bgUri) {
-        // Button to switch workspace
+    _createWorkspaceItem(index, isActive, globalBgUri) {
         let button = new St.Button({ reactive: true, can_focus: true });
         button.connect('clicked', () => {
             let ws = this.wm.get_workspace_by_index(index);
             if (ws) ws.activate(global.get_current_time());
         });
 
-        // Vertical wrapper for box + indicator
         let wrapper = new St.BoxLayout({ vertical: true, style_class: 'ow-ws-wrapper' });
         
         let box = new St.Bin({ style_class: `ow-ws-box ow-ws-box-${this.theme}` });
@@ -108,34 +153,20 @@ class OWWorkspaceSwitcher extends Applet.Applet {
             box.add_style_class_name('ow-ws-box-no-indicator');
         }
 
-        // LAPISAN 1: DIMMER 
         let dimmerBin = new St.Bin({ style_class: 'ow-ws-dimmer', x_fill: true, y_fill: true });
-
-        // LAPISAN 2: CENTER BIN
         let centerBin = new St.Bin({ x_align: St.Align.MIDDLE, y_align: St.Align.MIDDLE, x_fill: true, y_fill: true });
 
-        // Menyimpan semua inline CSS agar bisa diterapkan sekaligus
         let boxStyle = "";
 
-        // Jika opsi manual size diaktifkan, sesuaikan dengan status indikator
-        if (this.use_custom_size) {
-            if (this.show_indicator) {
-                // Ukuran saat indikator TAMPIL
-                boxStyle += `height: ${this.custom_height}px; width: ${this.custom_width}px; `;
-            } else {
-                // Ukuran saat indikator SEMBUNYI
-                boxStyle += `height: ${this.custom_height_no_indicator}px; width: ${this.custom_width_no_indicator}px; `;
-            }
-        }
-
-        // Jika opsi manual size diaktifkan
         if (this.use_custom_size) {
             boxStyle += `height: ${this.custom_height}px; width: ${this.custom_width}px; `;
         }
 
+        let boxBgUri = this._getWorkspaceWallpaper(index) || globalBgUri;
+
         if (this.overview_mode) {
-            if (bgUri) {
-                boxStyle += `background-image: url("${bgUri}"); background-size: cover; background-position: center;`;
+            if (boxBgUri) {
+                boxStyle += `background-image: url("${boxBgUri}"); background-size: cover; background-position: center;`;
             } else {
                 dimmerBin.remove_style_class_name('ow-ws-dimmer');
             }
@@ -166,7 +197,6 @@ class OWWorkspaceSwitcher extends Applet.Applet {
             centerBin.set_child(label);
         }
 
-        // Terapkan semua inline styles secara bersamaan (kustom height/width dan background)
         if (boxStyle !== "") {
             box.set_style(boxStyle);
         }
@@ -175,24 +205,18 @@ class OWWorkspaceSwitcher extends Applet.Applet {
         box.set_child(dimmerBin);
         wrapper.add_child(box);
 
-        // LOGIC INDIKATOR BARU
         if (this.show_indicator) {
             let indicatorArea = new St.Bin({ style_class: 'ow-ws-indicator-area', x_align: St.Align.MIDDLE, y_align: St.Align.MIDDLE });
             
             if (isActive) {
                 let indicatorWidget;
-                
                 if (this.indicator_style === 'triangle') {
-                    indicatorWidget = new St.Label({ 
-                        text: '▲', 
-                        style_class: `ow-ws-triangle-${this.theme}` 
-                    });
+                    indicatorWidget = new St.Label({ text: '▲', style_class: `ow-ws-triangle-${this.theme}` });
                 } else if (this.indicator_style === 'circle_filled') {
                     indicatorWidget = new St.Bin({ style_class: `ow-ws-circle-filled-${this.theme}` });
                 } else if (this.indicator_style === 'circle_empty') {
                     indicatorWidget = new St.Bin({ style_class: `ow-ws-circle-empty-${this.theme}` });
                 }
-
                 indicatorArea.set_child(indicatorWidget);
             }
             wrapper.add_child(indicatorArea);
